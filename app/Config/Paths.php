e63a206703f21f849cd160a16927b0409f94ecb2
dykim10 <dykim10iufc@gmail.com>
2025-03-06 20:58:56 +0900

    last commit

--- app/Config/Paths.php
+++ app/Config/Paths.php
@@ -72,4 +72,7 @@ class Paths
      * is used when no value is provided to `Services::renderer()`.
      */
     public string $viewDirectory = __DIR__ . '/../Views';
+
+    public string $publicDirectory = __DIR__;
+
 }
--- app/Filters/AuthFilter.php
+++ app/Filters/AuthFilter.php
@@ -10,7 +10,7 @@ class AuthFilter implements FilterInterface
 {
     public function before(RequestInterface $request, $arguments = null)
     {
-        if (!session()->get('logged_in')) {
+        if (!session()->get('isLoggedIn')) {
             return redirect()->to('/login')->with('error', '로그인이 필요합니다.');
         }
     }
